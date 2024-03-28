ae60e83ec108b3a6154f32f570a4563ab42528ea
Omar El Kamili <omar.elkamili@gmail.com>
2024-03-28 03:52:05 +0100

    add:styling con css

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 // ciclo for
 
 for (let i = 1; i <= 100; i++) {
-    let text;  
+    let text;
+    let color; 
     if (i % 3 === 0 && i % 5 === 0) { 
         text = "FizzBuzz";
+        color = "fizzbuzz";
     } else if (i % 3 === 0) { 
         text = "Fizz";
+        color = "fizz";
     } else if (i % 5 === 0) { 
         text = "Buzz";
+        color = "buzz";
     } else {
         text = i;
+        color = "default";
     }
 
 
@@ -21,7 +26,10 @@ for (let i = 1; i <= 100; i++) {
     // assegnazione alla variabile div il testo che deve indicare
     divElem.innerHTML = text;
 
+    // aggiunta delle classi nel div
+    divElem.classList.add("squareContainer");
+    divElem.classList.add(color);
     // aggiunta al file HTML
-    containerElem.append(divElem)
+    containerElem.append(divElem);
 
 }
